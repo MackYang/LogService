a6b76628d4c2c31f692306cb3dab881cc7c16f5e
Mack.Yang <1651493066@qq.com>
2018-12-02 11:51:33 +0800

    ref dll

--- LogSystem/UI/pages/Log.aspx.cs
+++ LogSystem/UI/pages/Log.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 
 namespace UI.PlatformUser
 {
-    public partial class SysLogList : System.Web.UI.Page
+    public partial class Log : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
